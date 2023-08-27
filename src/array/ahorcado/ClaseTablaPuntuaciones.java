25dc8a0450c442eaa9e88d42a2665f39ada410a5
jacs89 <116157054+jacs89@users.noreply.github.com>
2023-08-27 19:17:34 +0200

    El ahorcado ahora con puntuaciones

--- src/array/ahorcado/ClaseTablaPuntuaciones.java
+++ src/array/ahorcado/ClaseTablaPuntuaciones.java
@@ -1,13 +1,58 @@
 package array.ahorcado;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author Jose Cabello jcabello86@gmail.com
  */
 class ClaseTablaPuntuaciones {
 
-    void ejecutar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    private static final String RUTA_DE_ACCESO = "Puntuaciones/Puntuaciones.txt";
     
+    public void ejecutar() throws IOException {
+        
+        File archivoPuntuaciones = new File (RUTA_DE_ACCESO);
+        
+        if (!archivoPuntuaciones.exists() ){
+            crearArchivoPuntuaciones();
+            mostrarContenidoArchivoPuntuaciones();
+        }
+        else{
+            mostrarContenidoArchivoPuntuaciones();
+        }   
+    }
+
+    
+    private void crearArchivoPuntuaciones() throws IOException {
+        File carpetaPuntuaciones = new File ("Puntuaciones");
+        carpetaPuntuaciones.mkdirs();
+        
+        try (BufferedWriter escritoPuntuaciones = new BufferedWriter(new FileWriter (RUTA_DE_ACCESO) )){
+            System.out.println("El archivo de puntuaciones no existia. Ha sido creado.");
+            escritoPuntuaciones.write("No existen puntuaciones, juegue alguna partida. \n");            
+        }
+        catch (IOException e) {
+            System.out.println("Error al crear el archivo de puntuaciones.");
+        }
+    }
+
+    
+    private void mostrarContenidoArchivoPuntuaciones() {
+        try (BufferedReader lectorPuntuaciones = new BufferedReader(new FileReader (RUTA_DE_ACCESO) )) {
+            
+            String linea;
+            while ( (linea = lectorPuntuaciones.readLine() ) != null) {
+                System.out.println(linea);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error al leer el archivo de puntuaciones.");
+        }
+    }    
 }
--- src/array/ahorcado/MenuJuego.java
+++ src/array/ahorcado/MenuJuego.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * @version 1.6
+ * @version 1.6.1
  * @author Jose Antonio Cabello Santacruz
  * 
  * @changelog
+ * - Crear titulo del juego
  * - Se ha refactorizado el proyecto para mayor legibilidad, facilitar mantenimiento o nuevas implementaciones.
  * - Crear un menú previo donde:
  * - Seleccionar 2 jugadores.
@@ -29,8 +30,23 @@ public class MenuJuego {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
+        
         MenuJuego menu = new MenuJuego();
         menu.mostrarMenu();
+    }
+
+    private static void generarTitulo() {        
+
+        System.out.println("\n\n");
+        System.out.println("                ,,                                                      ,,             ");
+        System.out.println("      db      `7MM                                                    `7MM             ");
+        System.out.println("     ;MM:       MM                                                      MM             ");
+        System.out.println("    ,V^MM.      MMpMMMb.   ,pW''Wq.  `7Mb,od8   ,p60bo   ,60Yb.    ,M00bMM   ,pW0Wq.   ");
+        System.out.println("   ,M  `MM      MM    MM  6W'   `Wb   MM'  '  6M'   OO  8)   MM   ,AP    MM  6W'   `Wb ");
+        System.out.println("   AbmmmqMA     MM    MM  8M     M8   MM      8M         ,pm9MM   8MI    MM  8M     M8 ");
+        System.out.println("  A'     VML    MM    MM  YA.   ,A9   MM      YM.    ,  8M   MM   `Mb    MM  YA.   ,A9 ");
+        System.out.println(".AMA.   .AMMA..JMML  JMML. `Ybmd9'  .JMML.     YMbmd'  `Moo9^Yo.  `Wbmd\"MML. `Ybmd9'  ");
+        System.out.println("\n\n");
     }
     
     public void mostrarMenu() throws IOException {
@@ -38,6 +54,7 @@ public class MenuJuego {
             int opcion;
             
             do {
+                generarTitulo();
                 System.out.println("   MENÚ PRINCIPAL:");
                 System.out.println("0. Salir.");
                 System.out.println("1. Seleccionar 1 jugador.");
